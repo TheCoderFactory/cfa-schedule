daa7f7c9ce9609ea284009a6313a9e9244ae8c8a
Jordan Hart <jordan@jordanhart.com>
2016-03-30 16:10:03 +1100

    successfully setup with bangular

---
+++ client/views/home/home.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('cfaDashboard')
+  .controller('HomeCtrl', function () {
+
+    var vm = this;
+
+    angular.extend(vm, {
+      name: 'HomeCtrl'
+    });
+
+  });
---
+++ server/config/environment/development.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  mongo: {
+    uri: 'mongodb://localhost/cfa-schedule-dev'
+  }
+};
