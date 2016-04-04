7a3d0a487b52f70f7664bc5d27572202faca96b1
Jordan Hart <jordan@jordanhart.com>
2016-04-04 17:05:20 +1000

    faked routes so the app loads, but they need to be made real

--- server/api/intake/index.js
+++ server/api/intake/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var controller = require('./intake.controller');
 var auth = require('../../auth/auth.service');
 
-router.get('/intake', auth.isAuthenticated(), controller.getIntake);
+router.get('/intake', auth.isAuthenticated(), controller.create);
 router.post('/create-intake', controller.create);
 
 module.exports = router;
--- server/api/scheduled_item/index.js
+++ server/api/scheduled_item/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var controller = require('./scheduled_item.controller');
 var auth = require('../../auth/auth.service');
 
-router.get('/some-item', auth.isAuthenticated(), controller.getItem);
-router.post('/create-item', auth.isAuthenticated(), controller.createItem);
+router.get('/some-item', auth.isAuthenticated(), controller.create);
+router.post('/create-item', auth.isAuthenticated(), controller.create);
 
 module.exports = router;
