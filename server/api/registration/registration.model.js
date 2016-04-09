bed759a7811cc6a7cd05cca6b9556a4abaf2b20e
Simon Angell <simon.angell307@gmail.com>
2016-04-09 16:13:27 +1000

    added Reg files backend

---
+++ server/api/registration/registration.model.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var RegistrationsSchema = new Schema({
+  role: {type: String, required: true},
+  _intake: {type: Schema.Types.ObjectId, ref: 'Intake' required: true},
+});
+
+module.exports = mongoose.model('Registration', RegistrationsSchema);
