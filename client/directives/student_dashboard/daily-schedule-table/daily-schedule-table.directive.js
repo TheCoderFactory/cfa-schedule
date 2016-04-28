2a6e0d379dedfca07db076bef15d9df23732cc39
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-04-28 15:37:12 +1000

    scheduled items now appear on correct days in student dashboard

--- client/directives/student_dashboard/daily-schedule-table/daily-schedule-table.directive.js
+++ client/directives/student_dashboard/daily-schedule-table/daily-schedule-table.directive.js
@@ -1,45 +1,46 @@
 'use strict';
 
 angular.module('cfaDashboard')
-	.directive('dailyScheduleTable', ['$rootScope', 'DashboardService', function ($rootScope, DashboardService) {
-		return {
-			restrict: 'E',
-			templateUrl: 'directives/student_dashboard/daily-schedule-table/daily-schedule-table.html',
-			scope: {
-				date: '=', 
-				selectedScheduledItem: '=',
-				selectedScheduledItems: '='
-			},
-			link: function (scope, elem, attrs) {
-				
-				$rootScope.$on('ScheduledItemChanged', function () {
-					scope.selectedScheduledItems = scope.scheduledItems();
-				})
-				
-				scope.$watch('date', function () {
-					scope.selectedScheduledItems = scope.scheduledItems();
-				});
+.directive('dailyScheduleTable', ['$rootScope', 'DashboardService', function ($rootScope, DashboardService) {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/student_dashboard/daily-schedule-table/daily-schedule-table.html',
+    scope: {
+      date: '=', 
+      selectedScheduledItem: '=',
+      selectedScheduledItems: '='
+    },
+    link: function (scope, elem, attrs) {
 
-				scope.scheduledItems = function () {
-					
-					// select items that are on selected day
-					var dateItems = _.filter(DashboardService.settings.scheduledItems, function (scheduledItem) {
-					var selectedDate = moment(scope.date);
-					var startDate = moment(scheduledItem.start);
+      $rootScope.$on('ScheduledItemChanged', function () {
+        scope.selectedScheduledItems = scope.scheduledItems();
+      })
 
-					return selectedDate.diff(startDate, 'hours') < 25;
-				});
+      scope.$watch('date', function () {
+        scope.selectedScheduledItems = scope.scheduledItems();
+      });
 
-					// sort items by start date
-					var sortedDateItems = _.sortBy(dateItems, 'start');
-					return sortedDateItems;
-				};
+      scope.scheduledItems = function () {
 
-				scope.selectScheduledItem = function (scheduledItem) {
-					scope.selectedScheduledItem = scheduledItem;
-				};
+        // select items that are on selected day
+        var dateItems = _.filter(DashboardService.settings.scheduledItems, function (scheduledItem) {
+          var selectedDate = moment(scope.date);
+          var startDate = moment(scheduledItem.start);
+          var endDate = moment(scheduledItem.end);
 
-			}
+          return selectedDate.isSameOrAfter(startDate, 'day') && selectedDate.isSameOrBefore(endDate, 'day');
+        });
 
-		};
-	}]);
+        // sort items by start date
+        var sortedDateItems = _.sortBy(dateItems, 'start');
+        return sortedDateItems;
+      };
+
+      scope.selectScheduledItem = function (scheduledItem) {
+        scope.selectedScheduledItem = scheduledItem;
+      };
+
+    }
+
+  };
+}]);
