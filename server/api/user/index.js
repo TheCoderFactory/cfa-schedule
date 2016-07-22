c645beacb7a6fdc7500724bfcef29ef121d8c4e2
simon Angell <simonangell@simons-mbp.ait-sydney.net.au>
2016-07-22 16:06:18 +1000

    allowed unauthenticated user creation

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -7,7 +7,7 @@ var auth = require('../../auth/auth.service');
 var intake = require('../intake/intake.service');
 
 router.get('/me', auth.isAuthenticated(), controller.getMe);
-router.post('/', auth.isAuthenticated(), controller.create);
+router.post('/', controller.create);
 router.get('/exclude/:intakeId', auth.isAuthenticated(), controller.getUsersNotInIntake);
 router.get('/', auth.isAuthenticated(), controller.getAllUsers);
 router.get('/:userId', auth.isAuthenticated(), controller.getUser);
